a451a6b18609d13e8e70f3c987b84aefb4177325
Majd833 <lina.azzam.1611@gmail.com>
2024-11-11 17:41:43 +0200

    Country codes + Chack the Frequency

---
+++ Country Codes.py
@@ -0,0 +1,5 @@
+country_code={'India':'0091','Australia':"0025",'Nepal':'00977'}
+print("The Country code of India is:")
+print(country_code.get('India','Not Found'))
+print("\n The code for Japan is:")
+print(country_code.get("Japan","Not Found"))
